274c60de92259c41edd8dcc31d0fd18f60a7f4d7
Adam Rushford <177971875+Autodidac@users.noreply.github.com>
2026-08-04 04:23:49 -0400

    Chain final support pinning into v0.7.12 validation

--- tools/prepare_v0712_applicator.py
+++ tools/prepare_v0712_applicator.py
@@ -84,6 +84,7 @@ new_main = '''    patch_acceptance()
         "apply_v0712_round4.py",
         "apply_v0712_round5.py",
         "apply_v0712_round6.py",
+        "apply_v0712_round7.py",
     ):
         script = ROOT / "tools" / script_name
         namespace = {
