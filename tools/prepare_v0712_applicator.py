edbdad734a5504fc902a3fd4d506b0055c55bf2c
Adam Rushford <177971875+Autodidac@users.noreply.github.com>
2026-08-04 04:43:18 -0400

    Chain static press invalid-state correction

--- tools/prepare_v0712_applicator.py
+++ tools/prepare_v0712_applicator.py
@@ -87,6 +87,7 @@ new_main = '''    patch_acceptance()
         "apply_v0712_round7.py",
         "apply_v0712_round8.py",
         "apply_v0712_round9.py",
+        "apply_v0712_round10.py",
     ):
         script = ROOT / "tools" / script_name
         namespace = {
