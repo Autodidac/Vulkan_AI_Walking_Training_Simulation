8e6bfbe34c34dbca5e5f92c2a9ad6c9bddefa68d
Adam Rushford <177971875+Autodidac@users.noreply.github.com>
2026-08-04 08:08:58 -0400

    Chain final articulated-foot and static press gates

--- tools/prepare_v0712_applicator.py
+++ tools/prepare_v0712_applicator.py
@@ -113,6 +113,7 @@ new_main = '''    patch_acceptance()
         "apply_v0712_round10.py",
         "apply_v0712_round11.py",
         "apply_v0712_round12.py",
+        "apply_v0712_round13.py",
     ):
         script = ROOT / "tools" / script_name
         namespace = {
